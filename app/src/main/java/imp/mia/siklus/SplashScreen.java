2a7b482ab991c1ab9452b1fbe9e91a2e833efb95
IF-185 <if-185@gmail.com>
2022-10-04 20:56:33 +0700

    Add SplashScreen

---
+++ app/src/main/java/imp/mia/siklus/SplashScreen.java
@@ -0,0 +1,32 @@
+package imp.mia.siklus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Act Bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //DELAY
+    }
+}
